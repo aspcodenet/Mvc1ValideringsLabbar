b02b775fd193aea0aa62dd205f54a8835b4df4dd
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-01-26 15:07:07 +0100

    Add project files.

---
+++ Mvc1ValideringsLabbar/Controllers/DatumController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Mvc1ValideringsLabbar.ViewModels;
+
+namespace Mvc1ValideringsLabbar.Controllers
+{
+    public class DatumController : Controller
+    {
+        public IActionResult Index()
+        {
+            var viewModel = new DatumIndexViewModel();
+            viewModel.Datum = DateTime.Now;
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(DatumIndexViewModel viewModel)
+        {
+            if (viewModel.Month == 2 && viewModel.Day > 28)
+            {
+                ModelState.AddModelError("Day","Bara 28 dagar i fenriarti");
+            }
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ Mvc1ValideringsLabbar/ViewModels/DatumIndexViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mvc1ValideringsLabbar.ViewModels
+{
+    public class DatumIndexViewModel
+    {
+
+        [Range(1900, 2040, ErrorMessage = "Ange ett år YYYY")]
+        public int Year { get; set; }
+
+        [Range(1,12,ErrorMessage = "Felaktig månad. Ange värde mellan 1 och 12")]
+        public int Month { get; set; }
+
+        [Range(1, 31, ErrorMessage = "Dag mellan 1 och 31  IDIOT!!!")]
+        public int Day { get; set; }
+
+
+        [DataType(DataType.Date)]
+        public DateTime Datum { get; set; }
+
+
+    }
+}
